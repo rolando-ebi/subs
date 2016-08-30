62678e333aa8014f1573b790f5a5016234335601
David Richardson <davidr@ebi.ac.uk>
2016-08-30 15:15:22 +0100

    SUBS-202 improve dispatcher

--- subs-dispatcher/src/main/java/uk/ac/ebi/subs/dispatcher/DispatchProcessor.java
+++ subs-dispatcher/src/main/java/uk/ac/ebi/subs/dispatcher/DispatchProcessor.java
@@ -5,9 +5,14 @@ import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.stereotype.Service;
+import uk.ac.ebi.subs.data.component.SubsLink;
 import uk.ac.ebi.subs.data.submittable.Submission;
 import uk.ac.ebi.subs.data.submittable.Submittable;
 import uk.ac.ebi.subs.messaging.Channels;
+
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Set;
 
 
 @Service
@@ -16,54 +21,60 @@ public class DispatchProcessor {
     RabbitMessagingTemplate rabbitMessagingTemplate;
 
     @Autowired
-    public DispatchProcessor(RabbitMessagingTemplate rabbitMessagingTemplate, MessageConverter messageConverter){
+    public DispatchProcessor(RabbitMessagingTemplate rabbitMessagingTemplate, MessageConverter messageConverter) {
         this.rabbitMessagingTemplate = rabbitMessagingTemplate;
         this.rabbitMessagingTemplate.setMessageConverter(messageConverter);
     }
 
 
-    @RabbitListener(queues={Channels.SUBMISSION_SUBMITTED,Channels.SUBMISSION_PROCESSED})
-    public void handleSubmissionEvent(Submission submission){
-
+    @RabbitListener(queues = {Channels.SUBMISSION_SUBMITTED, Channels.SUBMISSION_PROCESSED})
+    public void handleSubmissionEvent(Submission submission) {
 
         /*
         * this is a deliberately simple implementation for prototyping
         * we will need to redo this as we flesh out the system
         * */
 
-        int sampleCount = 0;
+
+        /**
+         * for now, assume that anything with an accession is dealt with
+         */
+
+        long sampleCount = submission.getSamples().stream().filter(s -> (!s.isAccessioned())).count();
         int enaCount = 0;
         int arrayExpressCount = 0;
 
-        for (Submittable submittable : submission.allSubmissionItems()){
-            if (submittable.getAccession() != null && !submittable.getAccession().isEmpty()){
+        for (Submittable submittable : submission.allSubmissionItems()) {
+            if (submittable.isAccessioned()) {
                 continue;
             }
             switch (submittable.getRealm()) {
-                case Usi: sampleCount++; break;
-                case Arrays: arrayExpressCount++; break;
-                case Sequencing: enaCount++; break;
-                case FunctionalGenomics: arrayExpressCount++; break;
+                case Arrays:
+                    arrayExpressCount++;
+                    break;
+                case Sequencing:
+                    enaCount++;
+                    break;
+                case FunctionalGenomics:
+                    arrayExpressCount++;
+                    break;
+                default:
+                    break;
             }
         }
 
         String targetQueue = null;
 
-        if (sampleCount > 0){
+        if (sampleCount > 0) {
             targetQueue = Channels.SAMPLES_PROCESSING;
-        }
-        else if (enaCount > 0){
+        } else if (enaCount > 0) {
             targetQueue = Channels.ENA_PROCESSING;
-        }
-        else if (arrayExpressCount > 0){
+        } else if (arrayExpressCount > 0) {
             targetQueue = Channels.AE_PROCESSING;
         }
 
-        if (targetQueue != null){
-            rabbitMessagingTemplate.convertAndSend(targetQueue,submission);
+        if (targetQueue != null) {
+            rabbitMessagingTemplate.convertAndSend(targetQueue, submission);
         }
-
-
-
     }
 }
