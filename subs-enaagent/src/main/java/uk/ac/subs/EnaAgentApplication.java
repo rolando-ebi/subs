ac2a0f6ed3f1accb139acb97b95479215d9b7d43
David Richardson <davidr@ebi.ac.uk>
2016-08-31 11:36:41 +0100

    basics for prototype agent

---
+++ subs-enaagent/src/main/java/uk/ac/subs/EnaAgentApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.subs;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnaAgentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnaAgentApplication.class, args);
+    }
+}
---
+++ subs-enaagent/src/main/java/uk/ac/subs/enarepo/EnaAssayRepository.java
@@ -0,0 +1,9 @@
+package uk.ac.subs.enarepo;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import uk.ac.ebi.subs.data.submittable.Assay;
+import uk.ac.ebi.subs.data.submittable.Study;
+
+public interface EnaAssayRepository extends MongoRepository<Assay,String>{
+}
