21d81bcc647e65124b51ea7b836283a5e40a2b7d
fpenim <flaviapenim@gmail.com>
2016-09-08 09:25:50 +0100

    Updated ProgressMonitor class.

--- subs-progress-monitor/src/main/java/uk/ac/ebi/subs/progressmonitor/ProgressMonitor.java
+++ subs-progress-monitor/src/main/java/uk/ac/ebi/subs/progressmonitor/ProgressMonitor.java
@@ -1,16 +1,26 @@
 package uk.ac.ebi.subs.progressmonitor;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import uk.ac.ebi.subs.data.submittable.Submission;
+import uk.ac.ebi.subs.messaging.Channels;
 import uk.ac.ebi.subs.repository.SubmissionService;
+
+import java.util.List;
 
 @Component
 public class ProgressMonitor {
 
     private RabbitMessagingTemplate rabbitMessagingTemplate;
-
     private SubmissionService submissionService;
+
+    private boolean samplesStatus;
+    private boolean enaStatus;
+    private boolean aeStatus;
+
+    private List<Submission> submissions;
 
     @Autowired
     public ProgressMonitor(RabbitMessagingTemplate rabbitMessagingTemplate, SubmissionService submissionService) {
@@ -18,5 +28,16 @@ public class ProgressMonitor {
         this.submissionService = submissionService;
     }
 
+    @RabbitListener(queues = Channels.SUBMISSION_PROCESSED)
+    public boolean getSamplesStatus(Submission submission) {
+        return samplesStatus;
+    }
 
+    public boolean getEnaStatus() {
+        return enaStatus;
+    }
+
+    public boolean getAeStatus() {
+        return aeStatus;
+    }
 }
