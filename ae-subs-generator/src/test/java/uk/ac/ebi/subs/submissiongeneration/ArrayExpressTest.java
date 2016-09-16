7e61b9eae524ede7fc1edc0faa05a4fd0d9712c4
David Richardson <davidr@ebi.ac.uk>
2016-09-16 13:13:43 +0100

    remove guava dependency

--- ae-subs-generator/src/test/java/uk/ac/ebi/subs/submissiongeneration/ArrayExpressTest.java
+++ ae-subs-generator/src/test/java/uk/ac/ebi/subs/submissiongeneration/ArrayExpressTest.java
@@ -1,17 +1,18 @@
 package uk.ac.ebi.subs.submissiongeneration;
 
 
-import com.google.common.collect.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
@@ -40,15 +41,32 @@ public class ArrayExpressTest {
         List<Path> jsonFilePath = Files.walk(dir).filter(p -> p.toFile().isFile()).collect(Collectors.toList());
 
         assertThat("Files made", jsonFilePath.size(), equalTo(1));
+        assertThat("File name", jsonFilePath.get(0).toFile().getName(),equalTo("E-MTAB-4517.json"));
 
-        for (Path p : Lists.reverse(Files.walk(dir).collect(Collectors.toList()))){
-            Files.deleteIfExists(p);
-            System.out.println("deleted "+p);
-        }
+        Files.walkFileTree(dir, new FileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
 
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                System.out.println("deleted "+file);
+                return FileVisitResult.CONTINUE;
+            }
 
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path path, IOException exc) throws IOException {
+                Files.delete(path);
+                System.out.println("deleted "+path);
+                return FileVisitResult.CONTINUE;
+            }
+        });
     }
-
-
-
 }
