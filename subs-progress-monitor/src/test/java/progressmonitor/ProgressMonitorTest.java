f47994684014b35f97dbbfecf6b3c9c33cacf1ba
fpenim <flaviapenim@gmail.com>
2016-09-19 16:08:16 +0100

    Implemented equals(), hashCode() and toString() methods for Submission object.

---
+++ subs-progress-monitor/src/test/java/progressmonitor/ProgressMonitorTest.java
@@ -0,0 +1,7 @@
+package progressmonitor;
+
+/**
+ * Created by fpenim on 19/09/2016.
+ */
+public class ProgressMonitorTest {
+}
---
+++ subs-progress-monitor/src/test/java/util/Helpers.java
@@ -0,0 +1,7 @@
+package util;
+
+/**
+ * Created by fpenim on 19/09/2016.
+ */
+public class Helpers {
+}
