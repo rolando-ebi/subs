b7a9f6e854da125310348d55610cdd0177312f78
David Richardson <davidr@ebi.ac.uk>
2016-10-28 16:26:12 +0100

    WIP on new monitor implementation

--- subs-progress-monitor/src/main/java/uk/ac/ebi/subs/progressmonitor/QueueService.java
+++ subs-progress-monitor/src/main/java/uk/ac/ebi/subs/progressmonitor/QueueService.java
@@ -6,17 +6,18 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import uk.ac.ebi.subs.data.Submission;
 import uk.ac.ebi.subs.data.SubmissionEnvelope;
-import uk.ac.ebi.subs.data.submittable.*;
+import uk.ac.ebi.subs.messaging.Exchanges;
 import uk.ac.ebi.subs.messaging.Queues;
+import uk.ac.ebi.subs.messaging.Topics;
 import uk.ac.ebi.subs.processing.AgentResults;
+import uk.ac.ebi.subs.processing.Certificate;
 import uk.ac.ebi.subs.repository.SubmissionRepository;
 import uk.ac.ebi.subs.repository.submittable.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class QueueService {
@@ -53,86 +54,43 @@ public class QueueService {
     @RabbitListener(queues = Queues.SUBMISSION_MONITOR)
     public void checkForProcessedSubmissions(AgentResults agentResults) {
         //TODO reconstruct submission+envelope from repo
-        //TODO update repo with certs
-        //TODO send assembled submission+envelope to dispatcher
+        //TODO update repo with certs - done
+        //TODO send assembled submission+envelope to dispatcher - done for now
+        logger.info("received agent results for submission {} with {} certificates ",
+                agentResults.getSubmissionUuid(),agentResults.getCertificates().size());
 
-        Submission queueSubmission = submissionEnvelope.getSubmission();
+        Map<String,Certificate> certByUuid = new HashMap<>();
+        agentResults.getCertificates().forEach(c -> certByUuid.put(c.getUUID(),c));
 
-        logger.info("received submission {}, most recent handler was {}",
-                queueSubmission.getId(),
-                submissionEnvelope.mostRecentHandler());
-        Submission mongoSubmission = submissionRepository.findOne(queueSubmission.getId());
+        Submission submission = submissionRepository.findOne(agentResults.getSubmissionUuid());
 
-        if(checkForUpdates(queueSubmission, mongoSubmission)) {
-            //FIXME - Is this store/save doing an upsert? Check and fix if required.
-            saveSubmissionContents(mongoSubmission);
+        submission.allSubmissionItemsStream().forEach(s -> {
+                if (!certByUuid.containsKey(s.getId())) return;
 
-            logger.info("updated submission {}",queueSubmission.getId());
-        }
-        else {
-            logger.info("no changes for submission {}",queueSubmission.getId());
-        }
+                Certificate c = certByUuid.get(s.getId());
+                s.setAccession(c.getAccession());
+                s.setStatus(c.getProcessingStatus().toString());
+
+                logger.debug("Certificate {} applied to {}",c,s);
+            }
+        );
+
+        saveSubmissionContents(submission);
+
+        SubmissionEnvelope submissionEnvelope = new SubmissionEnvelope(submission);
+
+        rabbitMessagingTemplate.convertAndSend(
+                Exchanges.SUBMISSIONS,
+                Topics.EVENT_SUBMISSION_UPDATED,
+                submissionEnvelope
+        );
+
+        logger.info("submission {} updated",
+                submission.getId());
+
     }
 
-    private boolean checkForUpdates(Submission queueSubmission, Submission mongoSubmission) {
-        boolean updates = false;
 
-        List<Analysis> analyses = queueSubmission.getAnalyses();
-        if(!analyses.isEmpty() && !StringUtils.isEmpty(analyses.get(0).getAccession())) {
-            mongoSubmission.setAnalyses(analyses);
-            updates = true;
-        }
-
-        List<Assay> assays = queueSubmission.getAssays();
-        if(!assays.isEmpty() && !StringUtils.isEmpty(assays.get(0).getAccession())) {
-            mongoSubmission.setAssays(assays);
-            updates = true;
-        }
-
-        List<AssayData> assayData = new ArrayList<>();
-        if(!assayData.isEmpty() && !StringUtils.isEmpty(assayData.get(0).getAccession())) {
-            mongoSubmission.setAssayData(assayData);
-            updates = true;
-        }
-
-        List<EgaDac> egaDacs = new ArrayList<>();
-        if(!egaDacs.isEmpty() && !StringUtils.isEmpty(egaDacs.get(0).getAccession())) {
-            mongoSubmission.setEgaDacs(egaDacs);
-            updates = true;
-        }
-
-        List<EgaDacPolicy> egaDacPolicies = new ArrayList<>();
-        if(!egaDacPolicies.isEmpty() && !StringUtils.isEmpty(egaDacPolicies.get(0).getAccession())) {
-            mongoSubmission.setEgaDacPolicies(egaDacPolicies);
-            updates = true;
-        }
-
-        List<EgaDataset> egaDatasets = new ArrayList<>();
-        if(!egaDatasets.isEmpty() && !StringUtils.isEmpty(egaDatasets.get(0).getAccession())) {
-            mongoSubmission.setEgaDatasets(egaDatasets);
-            updates = true;
-        }
-
-        List<Project> projects = new ArrayList<>();
-        if(!projects.isEmpty() && !StringUtils.isEmpty(projects.get(0).getAccession())) {
-            mongoSubmission.setProjects(projects);
-            updates = true;
-        }
-
-        List<Sample> samples = queueSubmission.getSamples();
-        if(!samples.isEmpty() && !StringUtils.isEmpty(samples.get(0).getAccession())){
-            mongoSubmission.setSamples(samples);
-            updates = true;
-        }
-
-        List<Study> studies = new ArrayList<>();
-        if(!studies.isEmpty() && !StringUtils.isEmpty(studies.get(0).getAccession())){
-            mongoSubmission.setStudies(studies);
-            updates = true;
-        }
-
-        return updates;
-    }
 
     private void saveSubmissionContents(Submission submission) {
         analysisRepository.save(submission.getAnalyses());
