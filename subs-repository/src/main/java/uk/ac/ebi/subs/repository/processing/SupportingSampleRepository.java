98cd38a88e6d69968b793e2e4c3cab2a56ca1576
David Richardson <davidr@ebi.ac.uk>
2016-10-31 15:38:49 +0000

    clean off unnecessary imports

--- subs-repository/src/main/java/uk/ac/ebi/subs/repository/processing/SupportingSampleRepository.java
+++ subs-repository/src/main/java/uk/ac/ebi/subs/repository/processing/SupportingSampleRepository.java
@@ -1,12 +1,9 @@
 package uk.ac.ebi.subs.repository.processing;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
-import uk.ac.ebi.subs.data.Submission;
+
 
 import java.util.List;
 
