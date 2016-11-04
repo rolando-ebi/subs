a783cb06c54c4c8c9616b54513c7cae2be498324
David Richardson <davidr@ebi.ac.uk>
2016-11-04 14:27:54 +0000

    announce that samples are updated

--- subs-samples-agent/src/main/java/uk/ac/ebi/subs/samplesagent/SamplesListener.java
+++ subs-samples-agent/src/main/java/uk/ac/ebi/subs/samplesagent/SamplesListener.java
@@ -7,7 +7,7 @@ import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.stereotype.Service;
-import uk.ac.ebi.subs.processing.SubmissionEnvelope;
+import uk.ac.ebi.subs.processing.*;
 import uk.ac.ebi.subs.data.component.Archive;
 import uk.ac.ebi.subs.data.component.SampleRef;
 import uk.ac.ebi.subs.data.submittable.Sample;
@@ -15,9 +15,6 @@ import uk.ac.ebi.subs.data.Submission;
 import uk.ac.ebi.subs.messaging.Exchanges;
 import uk.ac.ebi.subs.messaging.Queues;
 import uk.ac.ebi.subs.messaging.Topics;
-import uk.ac.ebi.subs.processing.AgentResults;
-import uk.ac.ebi.subs.processing.Certificate;
-import uk.ac.ebi.subs.processing.ProcessingStatus;
 import uk.ac.ebi.subs.samplesrepo.SampleRepository;
 
 
@@ -75,11 +72,37 @@ public class SamplesListener {
 
     private List<Certificate> processSamples(Submission submission) {
         List<Certificate> certs = new ArrayList<>();
+        List<Sample> updatedSamples = new ArrayList<>();
 
         List<Sample> samples = submission.getSamples();
 
         samples.forEach(sample -> {
-            sample.setAccession(generateSampleAccession());
+
+            boolean isUpdate = false;
+
+            //if it already has an accession, it has been submitted before
+            if (sample.isAccessioned()){
+                isUpdate = true;
+            }
+
+            // if it has a domain and an alias, it might have been submitted already
+            if (sample.getDomain() != null && sample.getDomain().getName() != null && sample.getAlias() != null){
+                Sample existingSample = repository.findByDomainAndAlias(sample.getDomain().getName(),sample.getAlias());
+                if (existingSample != null){
+                    sample.setAccession(existingSample.getAccession());
+                    isUpdate = true;
+                }
+            }
+
+            //if it hasn't been submitted before, it needs an accession
+            if (!isUpdate){
+                sample.setAccession(generateSampleAccession());
+            }
+
+            // track that we're updating this sample, need to do something with it later
+            if (isUpdate){
+                updatedSamples.add(sample);
+            }
 
             sample.setStatus(ProcessingStatus.Processed.toString());
 
@@ -94,8 +117,25 @@ public class SamplesListener {
 
         repository.save(samples);
 
+        announceSampleUpdate(submission.getId(),updatedSamples);
+
+
         return certs;
     }
+
+    protected void announceSampleUpdate(String submissionId, List<Sample> updatedSamples){
+
+        if (updatedSamples.isEmpty()) return;
+
+        UpdatedSamplesEnvelope updatedSamplesEnvelope = new UpdatedSamplesEnvelope();
+        updatedSamplesEnvelope.setSubmissionId(submissionId);
+        updatedSamplesEnvelope.setUpdatedSamples(updatedSamples);
+
+        rabbitMessagingTemplate.convertAndSend(Exchanges.SUBMISSIONS,Topics.EVENT_SAMPLES_UPDATED,updatedSamplesEnvelope);
+
+    }
+
+
 
     protected void fillInSamples(SubmissionEnvelope envelope){
         //TODO this should do something in case of errors
--- subs-samples-agent/src/main/java/uk/ac/ebi/subs/samplesrepo/SampleRepository.java
+++ subs-samples-agent/src/main/java/uk/ac/ebi/subs/samplesrepo/SampleRepository.java
@@ -1,9 +1,15 @@
 package uk.ac.ebi.subs.samplesrepo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import uk.ac.ebi.subs.data.submittable.Sample;
 
 public interface SampleRepository extends MongoRepository<Sample, String> {
 
     Sample findByAccession(String accession);
+
+    @Query(value="{ 'domain.name' : ?0, 'alias' : ?1 }")
+    Sample findByDomainAndAlias(@Param(value="domainName") String domainName, @Param(value="alias") String alias);
+
 }
