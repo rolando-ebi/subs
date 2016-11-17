189ead39d29710879310cd5b3d10700034a2f18f
Flavia Penim <flaviapenim@gmail.com>
2016-11-17 17:03:15 +0000

    Added build.gradle file and Listener first methods.

--- subs-samples-agent/src/main/java/uk/ac/ebi/subs/agent/Listener.java
+++ subs-samples-agent/src/main/java/uk/ac/ebi/subs/agent/Listener.java
@@ -1,4 +1,34 @@
 package uk.ac.ebi.subs.agent;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.stereotype.Service;
+import uk.ac.ebi.subs.messaging.Queues;
+import uk.ac.ebi.subs.processing.SubmissionEnvelope;
+
+@Service
 public class Listener {
+    private static final Logger logger = LoggerFactory.getLogger(Listener.class);
+
+    private RabbitMessagingTemplate rabbitMessagingTemplate;
+
+    @Autowired
+    public Listener(RabbitMessagingTemplate rabbitMessagingTemplate, MessageConverter messageConverter) {
+        this.rabbitMessagingTemplate = rabbitMessagingTemplate;
+        this.rabbitMessagingTemplate.setMessageConverter(messageConverter);
+    }
+
+    @RabbitListener(queues = Queues.BIOSAMPLES_AGENT)
+    public void handleNewSamplesSubmission(SubmissionEnvelope envelope) {
+        // TODO - new submission
+    }
+
+    @RabbitListener(queues = Queues.SUBMISSION_NEEDS_SAMPLE_INFO)
+    public void fetchSupportSamplesRequested(SubmissionEnvelope envelope) {
+        // TODO - find and return existing samples requested
+    }
 }
