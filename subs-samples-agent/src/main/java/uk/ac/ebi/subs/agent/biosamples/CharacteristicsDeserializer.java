4207f6b6f582eded9c8ba0b8a89c046b03da4e18
Flavia Penim <flaviapenim@gmail.com>
2016-11-29 15:20:40 +0000

    Refactored CharacteristicsDeserializer to output List<Attributes>.

--- subs-samples-agent/src/main/java/uk/ac/ebi/subs/agent/biosamples/CharacteristicsDeserializer.java
+++ subs-samples-agent/src/main/java/uk/ac/ebi/subs/agent/biosamples/CharacteristicsDeserializer.java
@@ -6,45 +6,66 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.BaseJsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.node.TextNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class CharacteristicsDeserializer extends JsonDeserializer< List<Attribute>> {
+    private static final Logger logger = LoggerFactory.getLogger(CharacteristicsDeserializer.class);
 
     @Override
     public List<Attribute> deserialize(JsonParser parser, DeserializationContext ctxt) throws IOException {
         List<Attribute> attributes = new ArrayList<>();
-
-
-        // FIXME - deserialize into List<Attribute>
-        Map<String, List<String>> map = new HashMap<>();
+        Attribute attribute;
 
         JsonNode node = parser.getCodec().readTree(parser);
         Iterator jsonNodeIterator = node.fieldNames();
 
-        while (jsonNodeIterator.hasNext()) {
-            List<String> stringList = new ArrayList<>();
-
+        while (jsonNodeIterator.hasNext()) { // Characteristics iterator
             String characteristic = jsonNodeIterator.next().toString();
             ArrayNode an = (ArrayNode) node.get(characteristic);
 
             Iterator arrayNodeIterator = an.iterator();
-            while (arrayNodeIterator.hasNext()) {
+            while (arrayNodeIterator.hasNext()) { // Unwind multiple characteristics entries
+
+                attribute = new Attribute();
+                attribute.setName(characteristic);
+
                 ObjectNode on = (ObjectNode) arrayNodeIterator.next();
+                Iterator fieldNameIterator = on.fieldNames();
+                Iterator objectIterator = on.iterator();
 
-                on.forEach(o -> {
-                    if (o instanceof TextNode) {
-                        stringList.add(o.asText());
-                    } else if (o instanceof ArrayNode) {
-                        o.iterator().forEachRemaining(a -> stringList.add(a.asText()));
+                while (objectIterator.hasNext()) { // Extract characteristic structured content
+                    String fieldName = fieldNameIterator.next().toString();
+                    BaseJsonNode jsonNode = (BaseJsonNode) objectIterator.next();
+
+                    switch (fieldName) {
+                        case "text":
+                            attribute.setValue(jsonNode.asText());
+                            break;
+                        case "unit":
+                            attribute.setUnits(jsonNode.asText());
+                            break;
+                        case "ontologyTerms":
+                            ArrayNode arrayNode = (ArrayNode) jsonNode;
+                            List<String> ontoTerms = new ArrayList<>();
+                            arrayNode.iterator().forEachRemaining(o -> ontoTerms.add(o.asText()));
+                            attribute.setOntoTerms(ontoTerms);
+                            break;
+                        default:
+                            logger.warn("Unknown BioSamples characteristic [" + fieldName + "] skipping it.");
                     }
-                });
-            }
 
-            map.put(characteristic, stringList);
+                }
+
+                attributes.add(attribute);
+            }
         }
 
         return attributes;
