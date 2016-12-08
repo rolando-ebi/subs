a36f5539e709c88219c370b2f46bea2e48d66a8c
David Richardson <davidr@ebi.ac.uk>
2016-12-08 12:50:08 +0000

    don't expose repository by default

--- subs-frontend/src/main/java/uk/ac/ebi/subs/FrontendApplication.java
+++ subs-frontend/src/main/java/uk/ac/ebi/subs/FrontendApplication.java
@@ -3,10 +3,19 @@ package uk.ac.ebi.subs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy;
 
 @SpringBootApplication
 public class FrontendApplication {
     public static void main(String[] args) {
-        SpringApplication.run(FrontendApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(FrontendApplication.class, args);
+
+        RepositoryRestConfiguration restConfiguration = ctx.getBean(RepositoryRestConfiguration.class);
+
+        //TODO this property should be settable through the application.properties file in Spring BOOT 1.50
+
+        restConfiguration.setRepositoryDetectionStrategy(RepositoryDetectionStrategy.RepositoryDetectionStrategies.ANNOTATED);
     }
 }
