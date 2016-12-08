92c34390e82a4c2a7efabd2d4aea29137cf9b58a
David Richardson <davidr@ebi.ac.uk>
2016-12-08 12:49:39 +0000

    add submission resource assembler

--- subs-frontend/src/main/java/uk/ac/ebi/subs/frontend/ResourceAssemblerConfiguration.java
+++ subs-frontend/src/main/java/uk/ac/ebi/subs/frontend/ResourceAssemblerConfiguration.java
@@ -3,26 +3,47 @@ package uk.ac.ebi.subs.frontend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy;
 import org.springframework.hateoas.EntityLinks;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceAssembler;
-import org.springframework.stereotype.Component;
+import uk.ac.ebi.subs.data.Submission;
 import uk.ac.ebi.subs.data.submittable.Study;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @Configuration
 public class ResourceAssemblerConfiguration {
 
-    @Autowired
-    private EntityLinks entityLinks;
+    @Bean
+    ResourceAssembler<Submission, Resource<Submission>> submissionResourceAssembler() {
+        return submission -> {
+            Resource<Submission> submissionResource = new Resource<>(submission);
+
+            submissionResource.add(
+                    linkTo(
+                            methodOn(SubmissionController.class)
+                                    .getOne(submission.getDomain().getName(), submission.getId()
+                                    )
+                    ).withSelfRel()
+            );
+
+            //TODO add other relations
+
+            return submissionResource;
+        };
+    }
+
 
     @Bean
-    ResourceAssembler<Study,Resource<Study>> studyResourceAssembler(){
-        return entity -> {
-            Resource<Study> studyResource = new Resource<>(entity);
+    ResourceAssembler<Study, Resource<Study>> studyResourceAssembler() {
+        return study -> {
+            Resource<Study> studyResource = new Resource<>(study);
 
 
-            studyResource.add(entityLinks.linkForSingleResource(entity).withSelfRel());
+            //TODO add links
 
             return studyResource;
         };
