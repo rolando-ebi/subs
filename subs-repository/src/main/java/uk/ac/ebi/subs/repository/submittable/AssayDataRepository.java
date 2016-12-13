dbc575864e615c896736435cc7824edc4aed1880
David Richardson <davidr@ebi.ac.uk>
2016-12-13 10:51:04 +0000

    improve pluralisation with ego inflector + special case for assayData

--- subs-repository/src/main/java/uk/ac/ebi/subs/repository/submittable/AssayDataRepository.java
+++ subs-repository/src/main/java/uk/ac/ebi/subs/repository/submittable/AssayDataRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import uk.ac.ebi.subs.data.SubmissionLinks;
 import uk.ac.ebi.subs.data.submittable.Analysis;
@@ -11,6 +12,7 @@ import uk.ac.ebi.subs.data.submittable.AssayData;
 
 import java.util.List;
 
+@RepositoryRestResource( path="/assayData",collectionResourceRel = "assayData")//the plural of assay data is assay data#
 public interface AssayDataRepository extends MongoRepository<AssayData, String> {
 
     @RestResource(rel= SubmissionLinks.ASSAY_DATA)
