581fa47375a74b91e9debaef5b70c28494999118
David Richardson <davidr@ebi.ac.uk>
2016-12-19 15:53:36 +0000

    supply default error message, else you give a 500 error instead of 400 to the client

--- subs-frontend/src/main/java/uk/ac/ebi/subs/frontend/validators/AbstractSubmittableValidator.java
+++ subs-frontend/src/main/java/uk/ac/ebi/subs/frontend/validators/AbstractSubmittableValidator.java
@@ -10,7 +10,15 @@ import uk.ac.ebi.subs.data.Submission;
 import uk.ac.ebi.subs.data.submittable.Submittable;
 import uk.ac.ebi.subs.repository.SubmissionRepository;
 
-
+/**
+ * Base validator for submitted items
+ *
+ * Ensures that we have a submission ID and that it relates to a real submission
+ *
+ * Note that we must supply a default message. Not having a message causes the client to get a 500 (server error)
+ * status code instead of a 400 (bad request)
+ *
+ */
 public abstract class AbstractSubmittableValidator implements Validator {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -28,13 +36,13 @@ public abstract class AbstractSubmittableValidator implements Validator {
         logger.warn("validate {}",target);
         Submittable submittable = (Submittable)target;
 
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"submissionId","field.required");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"submissionId","field.required","submissionId is required");
 
         if (submittable.getSubmissionId() != null){
             Submission submission = submissionRepository.findOne(submittable.getSubmissionId());
 
             if (submission == null){
-                errors.rejectValue("submissionId","field.submissionNotFound");
+                errors.rejectValue("submissionId","field.submissionNotFound","submission not found for ID");
             }
         }
     }
