13b341583961f0c37b2c18c8dc0eea348b06186e
David Richardson <davidr@ebi.ac.uk>
2016-12-20 12:46:09 +0000

    clean up imports

--- subs-frontend/src/main/java/uk/ac/ebi/subs/frontend/config/RestRepositoryConfig.java
+++ subs-frontend/src/main/java/uk/ac/ebi/subs/frontend/config/RestRepositoryConfig.java
@@ -1,13 +1,8 @@
 package uk.ac.ebi.subs.frontend.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.rest.RepositoryRestProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import uk.ac.ebi.subs.data.Submission;
 
 @Configuration
