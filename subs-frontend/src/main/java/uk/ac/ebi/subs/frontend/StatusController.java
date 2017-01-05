bb5cd63b47af328fe96593853b1063533a1fa57b
David Richardson <davidr@ebi.ac.uk>
2017-01-05 12:02:46 +0000

    single status endpoints

--- subs-frontend/src/main/java/uk/ac/ebi/subs/frontend/StatusController.java
+++ subs-frontend/src/main/java/uk/ac/ebi/subs/frontend/StatusController.java
@@ -5,11 +5,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.ac.ebi.subs.data.status.Status;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @BasePathAwareController
@@ -27,9 +30,35 @@ public class StatusController {
         return new PageImpl<Status>(processingStatuses, pageable, processingStatuses.size());
     }
 
+    @RequestMapping("processingStatuses/{status}")
+    public Status processingStatus(@PathVariable String status){
+        Optional<Status> optionalStatus = processingStatuses.stream().filter(s -> s.getStatusName().equals(status))
+                .findFirst();
+
+        if (optionalStatus.isPresent()){
+            return optionalStatus.get();
+        }
+        else {
+            throw new ResourceNotFoundException();
+        }
+    }
+
     @RequestMapping("releaseStatuses")
     public Page<Status> allReleaseStatus(Pageable pageable) {
         return new PageImpl<Status>(releaseStatuses, pageable, releaseStatuses.size());
+    }
+
+    @RequestMapping("releaseStatuses/{status}")
+    public Status releaseStatus(@PathVariable String status){
+        Optional<Status> optionalStatus = releaseStatuses.stream().filter(s -> s.getStatusName().equals(status))
+                .findFirst();
+
+        if (optionalStatus.isPresent()){
+            return optionalStatus.get();
+        }
+        else {
+            throw new ResourceNotFoundException();
+        }
     }
 
 }
