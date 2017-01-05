f4f95e5029511be66c86fb4ee1a24f3776d28edb
David Richardson <davidr@ebi.ac.uk>
2017-01-05 16:50:34 +0000

    add processing status validatio

--- subs-frontend/src/main/java/uk/ac/ebi/subs/frontend/validators/ProjectValidator.java
+++ subs-frontend/src/main/java/uk/ac/ebi/subs/frontend/validators/ProjectValidator.java
@@ -3,18 +3,31 @@ package uk.ac.ebi.subs.frontend.validators;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.Errors;
-import org.springframework.validation.ValidationUtils;
-import org.springframework.validation.Validator;
+import uk.ac.ebi.subs.data.status.Status;
 import uk.ac.ebi.subs.data.submittable.Project;
+import uk.ac.ebi.subs.data.submittable.Submittable;
 import uk.ac.ebi.subs.repository.SubmissionRepository;
+import uk.ac.ebi.subs.repository.submittable.ProjectRepository;
+
+import java.util.Collection;
 
 @Component
-public class ProjectValidator extends AbstractSubmittableValidator{
+public class ProjectValidator extends AbstractSubmittableValidator {
+
+    private ProjectRepository projectRepository;
+
+    @Override
+    Submittable getCurrentVersion(String id) {
+        return projectRepository.findOne(id);
+    }
 
     @Autowired
-    public ProjectValidator(SubmissionRepository submissionRepository) {
-        super(submissionRepository);
+    public ProjectValidator(ProjectRepository projectRepository,
+                            SubmissionRepository submissionRepository,
+                            Collection<Status> processingStatuses,
+                            Collection<Status> releaseStatuses) {
+        super(submissionRepository, processingStatuses, releaseStatuses);
+        this.projectRepository = projectRepository;
     }
 
     @Override
--- subs-frontend/src/main/java/uk/ac/ebi/subs/frontend/validators/SampleValidator.java
+++ subs-frontend/src/main/java/uk/ac/ebi/subs/frontend/validators/SampleValidator.java
@@ -3,15 +3,32 @@ package uk.ac.ebi.subs.frontend.validators;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import uk.ac.ebi.subs.data.status.Status;
 import uk.ac.ebi.subs.data.submittable.Sample;
+import uk.ac.ebi.subs.data.submittable.Submittable;
 import uk.ac.ebi.subs.repository.SubmissionRepository;
+import uk.ac.ebi.subs.repository.submittable.SampleRepository;
+
+import java.util.Collection;
 
 @Component
 public class SampleValidator extends AbstractSubmittableValidator {
 
+    private SampleRepository sampleRepository;
+
+    @Override
+    Submittable getCurrentVersion(String id) {
+        return sampleRepository.findOne(id);
+    }
+
     @Autowired
-    public SampleValidator(SubmissionRepository submissionRepository) {
-        super(submissionRepository);
+    public SampleValidator(
+            SampleRepository sampleRepository,
+            SubmissionRepository submissionRepository,
+            Collection<Status> processingStatuses,
+            Collection<Status> releaseStatuses) {
+        super(submissionRepository, processingStatuses, releaseStatuses);
+        this.sampleRepository = sampleRepository;
     }
 
     @Override
