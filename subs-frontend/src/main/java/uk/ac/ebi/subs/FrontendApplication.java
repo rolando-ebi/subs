b7227d612099ffa2178097f4af29e47efd8307b9
Neil Goodgame <neilg@ebi.ac.uk>
2017-01-30 12:05:54 +0000

    Modified build so that it should still work with travis and added spring boot admin

--- subs-frontend/src/main/java/uk/ac/ebi/subs/FrontendApplication.java
+++ subs-frontend/src/main/java/uk/ac/ebi/subs/FrontendApplication.java
@@ -2,6 +2,7 @@ package uk.ac.ebi.subs;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -9,6 +10,7 @@ import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy;
 import uk.ac.ebi.subs.data.Submission;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class FrontendApplication {
     public static void main(String[] args) {
         SpringApplication.run(FrontendApplication.class, args);
