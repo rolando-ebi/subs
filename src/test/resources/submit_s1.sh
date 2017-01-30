8fdf4648bd7ecabdf8de93992a3cc2d154e31272
David Richardson <davidr@ebi.ac.uk>
2017-01-30 15:55:59 +0000

    full submission controller is a bad idea

--- src/test/resources/submit_s1.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# To use: start RabbitMQ, MongoDB, the FrontendApplication, the Dispatcher, then run this script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-curl localhost:8080/api/fullSubmissions -H "Content-Type: application/json" -X POST -d "@$DIR/s1.json" -v
