f12c212c6d1296e21d5405565727ad68128e98fb
David Richardson <davidr@ebi.ac.uk>
2017-01-31 15:14:09 +0000

    allow for type differences

--- subs-repository/src/main/java/uk/ac/ebi/subs/repository/FullSubmissionServiceImpl.java
+++ subs-repository/src/main/java/uk/ac/ebi/subs/repository/FullSubmissionServiceImpl.java
@@ -10,43 +10,53 @@ import uk.ac.ebi.subs.repository.repos.*;
 @Service
 public class FullSubmissionServiceImpl implements FullSubmissionService {
 
-    @Autowired  SubmissionRepository submissionRepository;
+    @Autowired
+    SubmissionRepository submissionRepository;
 
-    @Autowired  AnalysisRepository analysisRepository;
-    @Autowired  AssayDataRepository assayDataRepository;
-    @Autowired  AssayRepository assayRepository;
-    @Autowired  EgaDacPolicyRepository egaDacPolicyRepository;
-    @Autowired  EgaDacRepository egaDacRepository;
-    @Autowired  EgaDatasetRepository egaDatasetRepository;
-    @Autowired  ProjectRepository projectRepository;
-    @Autowired  ProtocolRepository protocolRepository;
-    @Autowired  SampleGroupRepository sampleGroupRepository;
-    @Autowired  SampleRepository sampleRepository;
-    @Autowired  StudyRepository studyRepository;
+    @Autowired
+    AnalysisRepository analysisRepository;
+    @Autowired
+    AssayDataRepository assayDataRepository;
+    @Autowired
+    AssayRepository assayRepository;
+    @Autowired
+    EgaDacPolicyRepository egaDacPolicyRepository;
+    @Autowired
+    EgaDacRepository egaDacRepository;
+    @Autowired
+    EgaDatasetRepository egaDatasetRepository;
+    @Autowired
+    ProjectRepository projectRepository;
+    @Autowired
+    ProtocolRepository protocolRepository;
+    @Autowired
+    SampleGroupRepository sampleGroupRepository;
+    @Autowired
+    SampleRepository sampleRepository;
+    @Autowired
+    StudyRepository studyRepository;
 
     @Override
     public FullSubmission fetchOne(String submissionId) {
         Submission minimalSub = submissionRepository.findOne(submissionId);
 
-        if (minimalSub == null){
+        if (minimalSub == null) {
             throw new ResourceNotFoundException();
         }
 
         FullSubmission submission = new FullSubmission(minimalSub);
 
-        /* TODO - FIX ME
-        submission.setAnalyses(analysisRepository.findBySubmissionId(submissionId));
-        submission.setAssayData(assayDataRepository.findBySubmissionId(submissionId));
-        submission.setAssays(assayRepository.findBySubmissionId(submissionId));
-        submission.setEgaDacPolicies(egaDacPolicyRepository.findBySubmissionId(submissionId));
-        submission.setEgaDacs(egaDacRepository.findBySubmissionId(submissionId));
-        submission.setEgaDatasets(egaDatasetRepository.findBySubmissionId(submissionId));
-        submission.setProjects(projectRepository.findBySubmissionId(submissionId));
-        submission.setProtocols(protocolRepository.findBySubmissionId(submissionId));
-        submission.setSampleGroups(sampleGroupRepository.findBySubmissionId(submissionId));
-        submission.setSamples(sampleRepository.findBySubmissionId(submissionId));
-        submission.setStudies(studyRepository.findBySubmissionId(submissionId));
-        */
+        submission.getAnalyses().addAll(analysisRepository.findBySubmissionId(submissionId));
+        submission.getAssayData().addAll(assayDataRepository.findBySubmissionId(submissionId));
+        submission.getAssays().addAll(assayRepository.findBySubmissionId(submissionId));
+        submission.getEgaDacPolicies().addAll(egaDacPolicyRepository.findBySubmissionId(submissionId));
+        submission.getEgaDacs().addAll(egaDacRepository.findBySubmissionId(submissionId));
+        submission.getEgaDatasets().addAll(egaDatasetRepository.findBySubmissionId(submissionId));
+        submission.getProjects().addAll(projectRepository.findBySubmissionId(submissionId));
+        submission.getProtocols().addAll(protocolRepository.findBySubmissionId(submissionId));
+        submission.getSampleGroups().addAll(sampleGroupRepository.findBySubmissionId(submissionId));
+        submission.getSamples().addAll(sampleRepository.findBySubmissionId(submissionId));
+        submission.getStudies().addAll(studyRepository.findBySubmissionId(submissionId));
 
         return submission;
     }
