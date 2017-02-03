136f62c1c785552a76d0541363a046d06a47b3b1
Neil Goodgame <neilg@ebi.ac.uk>
2017-02-03 17:05:01 +0000

    Added the ApplicationPidWriter so that springs process id is written out to application.pid

--- subs-frontend/src/main/java/uk/ac/ebi/subs/FrontendApplication.java
+++ subs-frontend/src/main/java/uk/ac/ebi/subs/FrontendApplication.java
@@ -3,6 +3,7 @@ package uk.ac.ebi.subs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy;
@@ -11,6 +12,9 @@ import uk.ac.ebi.subs.data.Submission;
 @SpringBootApplication
 public class FrontendApplication {
     public static void main(String[] args) {
-        SpringApplication.run(FrontendApplication.class, args);
+        SpringApplication springApplication = new SpringApplication( FrontendApplication.class);
+        ApplicationPidFileWriter applicationPidFileWriter = new ApplicationPidFileWriter();
+        springApplication.addListeners( applicationPidFileWriter );
+        springApplication.run(args);
     }
 }
