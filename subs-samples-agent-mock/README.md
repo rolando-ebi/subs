129263ff0e45789bb7e6779805046f55399c16fe
Flavia Penim <flaviapenim@gmail.com>
2017-02-06 17:25:51 +0000

    Removed subs-samples-agent-mock

--- subs-samples-agent-mock/README.md
+++
@@ -1,3 +0,0 @@
-##SUBS Samples Agent
-
-This prototype agent receives submissions from a queue, stores it into a mongoDB and returns the submission with accessions for the submitted samples.
--- subs-samples-agent-mock/src/test/java/uk/ac/ebi/subs/samplesrepo/SampleRepositoryTestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.ebi.subs.samplesrepo;
-
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableAutoConfiguration
-public class SampleRepositoryTestConfiguration {
-}
