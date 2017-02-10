add5c5d649fc1c334c10b048c7f6667ceef282ea
David Richardson <davidr@ebi.ac.uk>
2017-02-10 15:06:02 +0000

    domain controller and processor

--- subs-api/src/main/java/uk/ac/ebi/subs/api/DomainController.java
+++ subs-api/src/main/java/uk/ac/ebi/subs/api/DomainController.java
@@ -1,15 +1,22 @@
 package uk.ac.ebi.subs.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.Resource;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriUtils;
+import uk.ac.ebi.subs.api.resourceAssembly.SimpleResourceAssembler;
+import uk.ac.ebi.subs.data.Submission;
 import uk.ac.ebi.subs.data.component.Domain;
+import uk.ac.ebi.subs.repository.SubmissionRepository;
 
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
@@ -18,6 +25,12 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 @RestController
 @BasePathAwareController
 public class DomainController {
+
+    @Autowired
+    private SubmissionRepository submissionRepository;
+    @Autowired
+    private SimpleResourceAssembler<Submission> simpleResourceAssembler;
+
 
     @RequestMapping("/domains/{domainName}")
     public Resource<Domain> getDomain(@PathVariable String domainName) throws UnsupportedEncodingException {
@@ -32,12 +45,22 @@ public class DomainController {
         resource.add(
                 linkTo(
                         methodOn(this.getClass()).getDomain(
-                                 d.getName()
+                                d.getName()
                         )
                 ).withSelfRel()
         );
 
         return resource;
+    }
+
+    @RequestMapping("/domains/{domainName}/submissions")
+    public PagedResources<Resource<Submission>> domainSubmissions(
+            @PathVariable String domainName,
+            Pageable pageable,
+            PagedResourcesAssembler<Submission> pagedResourcesAssembler) {
+        Page<Submission> page = submissionRepository.findByDomainName(domainName, pageable);
+
+        return pagedResourcesAssembler.toResource(page, simpleResourceAssembler);
     }
 
 }
