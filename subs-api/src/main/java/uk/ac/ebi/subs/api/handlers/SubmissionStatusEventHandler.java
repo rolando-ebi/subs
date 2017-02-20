86b3da697d052b720a2931ebf05b9a0b0834ca9a
David Richardson <davidr@ebi.ac.uk>
2017-02-20 12:39:19 +0000

    submission status handler starts the submission process

--- subs-api/src/main/java/uk/ac/ebi/subs/api/handlers/SubmissionStatusEventHandler.java
+++ subs-api/src/main/java/uk/ac/ebi/subs/api/handlers/SubmissionStatusEventHandler.java
@@ -1,13 +1,16 @@
 package uk.ac.ebi.subs.api.handlers;
 
-import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.HandleBeforeSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.subs.api.services.SubmissionEventService;
 import uk.ac.ebi.subs.data.status.SubmissionStatusEnum;
 import uk.ac.ebi.subs.repository.SubmissionRepository;
+import uk.ac.ebi.subs.repository.model.Submission;
 import uk.ac.ebi.subs.repository.model.SubmissionStatus;
+
+import java.util.Date;
 
 /**
  * Created by davidr on 20/02/2017.
@@ -19,14 +22,21 @@ public class SubmissionStatusEventHandler {
     private SubmissionRepository submissionRepository;
     private SubmissionEventService submissionEventService;
 
-    public SubmissionStatusEventHandler(SubmissionRepository submissionRepository){
+    @Autowired
+    public SubmissionStatusEventHandler(SubmissionRepository submissionRepository, SubmissionEventService submissionEventService) {
         this.submissionRepository = submissionRepository;
+        this.submissionEventService = submissionEventService;
     }
 
     @HandleBeforeSave
-    public void handleUpdate(SubmissionStatus submissionStatus){
-        if (SubmissionStatusEnum.Submitted.name().equals(submissionStatus.getStatus())){
+    public void handleUpdate(SubmissionStatus submissionStatus) {
+        if (SubmissionStatusEnum.Submitted.name().equals(submissionStatus.getStatus())) {
+            Submission submission = submissionRepository.findBySubmissionStatusId(submissionStatus.getId());
 
+            submissionEventService.submissionSubmitted(submission);
+
+            submission.setSubmissionDate(new Date());
+            submissionRepository.save(submission);
         }
     }
 }
