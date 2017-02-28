0beae3b3fb40931eb15bf4b74228274761540d25
David Richardson <davidr@ebi.ac.uk>
2017-02-28 15:18:39 +0000

    status processor must be annotated to be used

--- subs-api/src/main/java/uk/ac/ebi/subs/api/resourceAssembly/SubmissionStatusResourceProcessor.java
+++ subs-api/src/main/java/uk/ac/ebi/subs/api/resourceAssembly/SubmissionStatusResourceProcessor.java
@@ -2,12 +2,14 @@ package uk.ac.ebi.subs.api.resourceAssembly;
 
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.stereotype.Component;
 import uk.ac.ebi.subs.api.controllers.StatusDescriptionController;
 import uk.ac.ebi.subs.repository.model.SubmissionStatus;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+@Component
 public class SubmissionStatusResourceProcessor implements ResourceProcessor<Resource<SubmissionStatus>> {
 
     @Override
