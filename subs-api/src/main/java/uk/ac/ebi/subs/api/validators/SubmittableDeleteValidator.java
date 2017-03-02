10027db4ad572966a00606b59dd8c3f14fd6f82c
David Richardson <davidr@ebi.ac.uk>
2017-03-02 16:24:06 +0000

    use operation control service for delete validation

--- subs-api/src/main/java/uk/ac/ebi/subs/api/validators/SubmittableDeleteValidator.java
+++ subs-api/src/main/java/uk/ac/ebi/subs/api/validators/SubmittableDeleteValidator.java
@@ -3,11 +3,19 @@ package uk.ac.ebi.subs.api.validators;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
+import uk.ac.ebi.subs.api.services.OperationControlService;
 import uk.ac.ebi.subs.data.status.ProcessingStatusEnum;
 import uk.ac.ebi.subs.repository.model.StoredSubmittable;
 
 @Component
 public class SubmittableDeleteValidator implements Validator {
+
+    private OperationControlService operationControlService;
+
+    public SubmittableDeleteValidator(OperationControlService operationControlService) {
+        this.operationControlService = operationControlService;
+    }
+
     @Override
     public boolean supports(Class<?> clazz) {
         return StoredSubmittable.class.isAssignableFrom(clazz);
@@ -18,9 +26,10 @@ public class SubmittableDeleteValidator implements Validator {
         StoredSubmittable submittable = (StoredSubmittable) target;
 
 
-        if (!ProcessingStatusEnum.Draft.name().equals(submittable.getProcessingStatus().getStatus())) {
+        if (!operationControlService.isUpdateable(submittable)) {
             SubsApiErrors.resource_locked.addError(errors);
         }
+        //TODO could also lock out deletion of objects that are referenced, but need to consider the rules carefully
 
     }
 }
