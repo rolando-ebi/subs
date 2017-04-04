ff277ec46e3aedb2abe36591189da5b0e5742477
Flavia Penim <flaviapenim@gmail.com>
2017-04-04 15:30:40 +0100

    Reimplemented UpdateService using biosamples client.

--- subs-samples-agent/src/main/java/uk/ac/ebi/subs/agent/services/UpdateService.java
+++ subs-samples-agent/src/main/java/uk/ac/ebi/subs/agent/services/UpdateService.java
@@ -9,10 +9,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import uk.ac.ebi.biosamples.client.BioSamplesClient;
 import uk.ac.ebi.subs.agent.converters.BsdSampleToUsiSample;
 import uk.ac.ebi.subs.agent.converters.UsiSampleToBsdSample;
 import uk.ac.ebi.subs.data.status.ProcessingStatus;
@@ -27,43 +29,25 @@ public class UpdateService {
     private static final Logger logger = LoggerFactory.getLogger(UpdateService.class);
 
     @Autowired
-    private RestTemplate restTemplate;
+    BioSamplesClient client;
 
     @Autowired
     UsiSampleToBsdSample toBsdSample;
     @Autowired
     BsdSampleToUsiSample toUsiSample;
 
-    private String apiUrl;
-
     public void update(List<Sample> sampleList) {
         sampleList.forEach(usiSample -> {
-            uk.ac.ebi.biosamples.model.Sample bsdSample = toBsdSample.convert(usiSample);
-            /*if(update(bsdSample)) {
-                usiSample.setStatus(ProcessingStatus.Done); FIXME
-            } else {
-                usiSample.setStatus(ProcessingStatus.Error);
-            }*/
+            update(toBsdSample.convert(usiSample));
         });
     }
 
     private boolean update(uk.ac.ebi.biosamples.model.Sample bsdSample) {
-        URI uri = UriComponentsBuilder
-                .fromUriString(apiUrl)
-                .path(bsdSample.getAccession())
-                .build()
-                .toUri();
-        logger.info("URI: " + uri);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        RequestEntity<uk.ac.ebi.biosamples.model.Sample> requestEntity;
+        logger.info("Updating sample.");
 
         try {
-            requestEntity = new RequestEntity<>(bsdSample, headers, HttpMethod.PUT, uri);
-            restTemplate.exchange(requestEntity, uk.ac.ebi.biosamples.model.Sample.class);
-
-        } catch (HttpServerErrorException e) {
+            client.persist(bsdSample);
+        } catch (HttpClientErrorException e) {
             logger.error("Update [" + bsdSample.getAccession() + "] failed with error:", e);
             return false;
         } catch (ResourceAccessException e) {
@@ -71,13 +55,5 @@ public class UpdateService {
             return false;
         }
         return true;
-    }
-
-    public String getApiUrl() {
-        return apiUrl;
-    }
-
-    public void setApiUrl(String apiUrl) {
-        this.apiUrl = apiUrl;
     }
 }
--- subs-samples-agent/src/test/java/uk/ac/ebi/subs/agent/services/UpdateServiceTest.java
+++ subs-samples-agent/src/test/java/uk/ac/ebi/subs/agent/services/UpdateServiceTest.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.subs.agent.services;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -9,7 +10,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
+import uk.ac.ebi.biosamples.client.BioSamplesClient;
+import uk.ac.ebi.biosamples.client.BioSamplesClientConfig;
+import uk.ac.ebi.biosamples.client.ClientProperties;
 import uk.ac.ebi.subs.BioSamplesDependentTest;
 import uk.ac.ebi.subs.agent.converters.BsdAttributeToUsiAttribute;
 import uk.ac.ebi.subs.agent.converters.BsdRelationshipToUsiRelationship;
@@ -31,8 +36,11 @@ import java.util.Arrays;
         BsdSampleToUsiSample.class,
         BsdAttributeToUsiAttribute.class,
         BsdRelationshipToUsiRelationship.class,
-        RestTemplate.class,
-        TestUtils.class
+        TestUtils.class,
+        BioSamplesClient.class,
+        ClientProperties.class,
+        BioSamplesClientConfig.class,
+        RestOperations.class
 })
 @ConfigurationProperties(prefix = "test")
 @EnableAutoConfiguration
@@ -54,13 +62,6 @@ public class UpdateServiceTest {
     @Test
     @Category(BioSamplesDependentTest.class)
     public void update() {
-        //updateService.update(Arrays.asList(sample));
-    }
-
-    @Test
-    @Category(BioSamplesDependentTest.class)
-    public void updateFail() {
-        //sample.setAccession("FAIL");
         //updateService.update(Arrays.asList(sample));
     }
 
