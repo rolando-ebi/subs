ee610e5437bbe03c13ed018856585d346120bd66
Neil Goodgame <neilg@ebi.ac.uk>
2017-04-08 21:37:28 +0100

    disabling autoconfiguration for mongodb as it results in the agents trying to connect to mongo
    http://stackoverflow.com/questions/28747909/how-to-disable-spring-data-mongodb-autoconfiguration-in-spring-boot

--- subs-messaging/src/main/java/uk/ac/ebi/subs/MessagingApplication.java
+++ subs-messaging/src/main/java/uk/ac/ebi/subs/MessagingApplication.java
@@ -2,8 +2,10 @@ package uk.ac.ebi.subs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class MessagingApplication {
     public static void main(String[] args) {
         SpringApplication.run(MessagingApplication.class, args);
